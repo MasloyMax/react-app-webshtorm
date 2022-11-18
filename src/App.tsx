5f0349cb82cffb12c637b0b378529b4e54eaece5
MasloyMax <masloymax@gmail.com>
2022-11-19 00:44:45 +0300

    Commit - 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Lorem  insum dolor</p>
+        <p>Lorem  insum dolor</p>
         <p>Lorem  insum dolor</p>
         <a
           className="App-link"
